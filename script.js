f4c756b222cc8a50e98626e6914422daa4f1c019
ECalan <tecnoelectronictrance@gmail.com>
2023-02-04 21:14:08 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+function botonEncriptar(){
+    document.querySelector(".contenidoCuadro").style.display ="none";
+    document.querySelector(".areaDeCifrado").style.display ="inline-block";
+    var texto = document.getElementsByClassName("textoIngresado")[0].value;
+    var mapearObjeto = {a:"ai", e:"enter", i:"imes", o:"ober", u:"ufat"};
+    texto = texto.replace(/a|e|i|o|u/g, function(encontrado){return mapearObjeto[encontrado]})
+    document.querySelector(".mensaje").value = (texto);
+    document.querySelector(".textoIngresado").value ="";
+}
+
+function botonDesencriptar(){
+    var textoIngresado = document.getElementsByClassName("textoIngresado")[0].value;
+    var mapearObjeto = {ai:"a", enter:"e", imes:"i", ober:"o", ufat:"u"};
+    textoIngresado = textoIngresado.replace(/ai|enter|imes|ober|ufat/g, function(encontrado){return mapearObjeto[encontrado]})
+    document.querySelector(".mensaje").value = (textoIngresado);
+    document.querySelector(".textoIngresado").value ="";
+}
+
+function botonCopiar(){
+    var contenido = document.getElementsByClassName("mensaje")[0].value;
+    
+    if (contenido == ""){
+        Swal.fire ({title: 'No se encontraron datos',
+        icon: "error",
+        color: '#0A3871',
+        backdrop: `rgba(0,0,0,0.4)`,
+        showConfirmButton: false,
+        timer: 1100});
+    }
+    else{
+        navigator.clipboard.writeText(contenido)
+        document.querySelector(".mensaje").value ="";
+        Swal.fire ({title: 'Copiado',
+        icon: "success",
+        color: '#0A3871',
+        backdrop: `rgba(0,0,0,0.4)`,
+        showConfirmButton: false,
+        timer: 1100});
+    }
+    
+}
+
+encriptar.onclick = botonEncriptar;
+desencriptar.onclick = botonDesencriptar;
+copiar.onclick = botonCopiar;
