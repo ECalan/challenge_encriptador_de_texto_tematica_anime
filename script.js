ec67d4c24a327282b882af01bba76bace5738409
ECalan <tecnoelectronictrance@gmail.com>
2023-02-05 19:05:42 -0600

    se coloco una condicion y un Swal.fire

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 
 function botonEncriptar(){
-    document.querySelector(".contenidoCuadro").style.display ="none";
-    document.querySelector(".areaDeCifrado").style.display ="inline-block";
     var texto = document.getElementsByClassName("textoIngresado")[0].value;
     var mapearObjeto = {a:"ai", e:"enter", i:"imes", o:"ober", u:"ufat"};
+
+    if(texto == ""){
+        Swal.fire ({title: 'Por favor ingrese texto',
+        icon: "warning",
+        color: '#0A3871',
+        backdrop: `rgba(0,0,0,0.4)`,
+        showConfirmButton: false,
+        timer: 1150});
+    }
+    else{
+    document.querySelector(".contenidoCuadro").style.display ="none";
+    document.querySelector(".areaDeCifrado").style.display ="inline-block";   
     texto = texto.replace(/a|e|i|o|u/g, function(encontrado){return mapearObjeto[encontrado]})
     document.querySelector(".mensaje").value = (texto);
     document.querySelector(".textoIngresado").value ="";
+    }
 }
 
 function botonDesencriptar(){
